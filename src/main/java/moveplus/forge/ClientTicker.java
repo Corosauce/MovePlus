f2d8baf238e846cf0dbb3419960a565799600c6e
Corosauce <cordonfreeman@gmail.com>
2019-01-05 17:59:21 +0000

    Add backwards behind feet check and onGround check to minimize bounce problem

--- src/main/java/moveplus/forge/ClientTicker.java
+++ src/main/java/moveplus/forge/ClientTicker.java
@@ -20,12 +20,15 @@ public class ClientTicker {
         EntityPlayer player = Minecraft.getMinecraft().player;
         Entity camera = Minecraft.getMinecraft().getRenderViewEntity();
 
+        boolean renderDebug = false;
+
         if (player == null || camera == null) return;
 
-        if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)) {
+        if (Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)/* && !player.onGround*/) {
 
             float grabDist = 0.75F;
             Vec3d lookVec = player.getLookVec().scale(grabDist);
+            Vec3d lookVecBehind = player.getLookVec().scale(-0.25F);
 
             //from where they are grabbing along x and z
             //now we will scan from up to down, seeing if theres a spot there hands or feet could be
@@ -40,32 +43,42 @@ public class ClientTicker {
             double yScanRes = 0.2D;
             double yAirSize = 0.25D;
             double xzSize = 0.3D;
+            double xzSizeBehind = 0.1D;
 
             AxisAlignedBB playerAABB = player.getEntityBoundingBox();
             AxisAlignedBB spotForHandsAir = new AxisAlignedBB(player.posX + lookVec.x, playerAABB.minY, player.posZ + lookVec.z,
                     player.posX + lookVec.x, playerAABB.minY, player.posZ + lookVec.z)
                     .grow(xzSize, yAirSize, xzSize);
 
+            AxisAlignedBB behindUnderFeet = new AxisAlignedBB(player.posX + lookVecBehind.x, playerAABB.minY, player.posZ + lookVecBehind.z,
+                    player.posX + lookVecBehind.x, playerAABB.minY, player.posZ + lookVecBehind.z)
+                    .grow(xzSizeBehind, xzSizeBehind, xzSizeBehind);
+
+            if (renderDebug) renderOffsetAABB(behindUnderFeet.offset(-player.posX, -playerAABB.minY, -player.posZ), 0, 0, 0, 0, 1, 0);
+
             //initial air finding loop
             boolean foundGrabbableSpot = false;
-            for (double y = yScanRangeAir; y > 0.25D && !foundGrabbableSpot; y -= yScanRes) {
-                //if found a good air spot, find a solid spot under it within a tiny range of it
-                if (player.world.getCollisionBoxes(player, spotForHandsAir.offset(0, y, 0)).size() == 0) {
-                    //TEMP
-                    //foundGrabbableSpot = true;
-                    AxisAlignedBB aabbRenderAir = spotForHandsAir.offset(-player.posX, -playerAABB.minY + y, -player.posZ);
-                    renderOffsetAABB(aabbRenderAir.grow(xzSize, 0, xzSize), 0, 0, 0, 0, 0, 1);
-                    for (double y2 = 0; y2 < yScanRangeSolid; y2 += yScanRes) {
-                        //start the spot half intersecting the air spot and iterate down a bit
-                        AxisAlignedBB aabbTry2 = spotForHandsAir.offset(0, y - (yAirSize * 1D) - y2, 0);
-                        AxisAlignedBB aabbRenderSolid = aabbTry2.offset(-player.posX, -playerAABB.minY, -player.posZ);
-                        AxisAlignedBB aabb2 = new AxisAlignedBB(0, 0, 0, 0, 0, 0).grow(1, 1, 1);
-                        if (player.world.getCollisionBoxes(player, aabbTry2).size() > 0 && aabbTry2.minY + 0.15D > playerAABB.minY) {
-                            foundGrabbableSpot = true;
-                            renderOffsetAABB(aabbRenderSolid.grow(xzSize, 0, xzSize), 0, 0, 0, 1, 0, 0);
-                            //Render.renderOffsetAABB(aabbRender, -camera.posX, -camera.posY, -camera.posZ);
-                            //Render.renderOffsetAABB(aabb2, 0, 0, 0);
-                            break;
+            //fix it trying to climb while either on the ground or just getting over ledge
+            if (!player.onGround && player.world.getCollisionBoxes(player, behindUnderFeet).size() == 0) {
+                for (double y = yScanRangeAir; y > 0.25D && !foundGrabbableSpot; y -= yScanRes) {
+                    //if found a good air spot, find a solid spot under it within a tiny range of it
+                    if (player.world.getCollisionBoxes(player, spotForHandsAir.offset(0, y, 0)).size() == 0) {
+                        //TEMP
+                        //foundGrabbableSpot = true;
+                        AxisAlignedBB aabbRenderAir = spotForHandsAir.offset(-player.posX, -playerAABB.minY + y, -player.posZ);
+                        if (renderDebug) renderOffsetAABB(aabbRenderAir.grow(xzSize, 0, xzSize), 0, 0, 0, 0, 0, 1);
+                        for (double y2 = 0; y2 < yScanRangeSolid; y2 += yScanRes) {
+                            //start the spot half intersecting the air spot and iterate down a bit
+                            AxisAlignedBB aabbTry2 = spotForHandsAir.offset(0, y - (yAirSize * 1D) - y2, 0);
+                            AxisAlignedBB aabbRenderSolid = aabbTry2.offset(-player.posX, -playerAABB.minY, -player.posZ);
+                            AxisAlignedBB aabb2 = new AxisAlignedBB(0, 0, 0, 0, 0, 0).grow(1, 1, 1);
+                            if (player.world.getCollisionBoxes(player, aabbTry2).size() > 0 && aabbTry2.minY + 0.15D > playerAABB.minY) {
+                                foundGrabbableSpot = true;
+                                if (renderDebug) renderOffsetAABB(aabbRenderSolid.grow(xzSize, 0, xzSize), 0, 0, 0, 1, 0, 0);
+                                //Render.renderOffsetAABB(aabbRender, -camera.posX, -camera.posY, -camera.posZ);
+                                //Render.renderOffsetAABB(aabb2, 0, 0, 0);
+                                break;
+                            }
                         }
                     }
                 }
