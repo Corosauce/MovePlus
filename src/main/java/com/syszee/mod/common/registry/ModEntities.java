21035ac57c165f7339045558fb104bdb9d4c1656
Ocelot <FinntheRaider@gmail.com>
2020-10-21 07:14:28 -0700

    Removed item properties from entity registry method

--- src/main/java/com/syszee/mod/common/registry/ModEntities.java
+++ src/main/java/com/syszee/mod/common/registry/ModEntities.java
@@ -5,6 +5,7 @@ import io.github.ocelot.sonar.common.item.SpawnEggItemBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
@@ -17,13 +18,13 @@ public class ModEntities
 {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, ModMain.MOD_ID);
 
-    // public static final RegistryObject<EntityType<ExampleEntity>> EXAMPLE = register("example", EntityType.Builder.create(ExampleEntity::new, EntityClassification.MISC).size(1.0F, 1.0F).trackingRange(10), 0xff00ff, 0xff00ff, new Item.Properties().group(ItemGroup.MISC));
+    // public static final RegistryObject<EntityType<ExampleEntity>> EXAMPLE = register("example", EntityType.Builder.create(ExampleEntity::new, EntityClassification.MISC).size(1.0F, 1.0F).trackingRange(10), 0xff00ff, 0xff00ff);
 
     @SubscribeEvent
     public static void onEvent(BiomeLoadingEvent event)
     {
         // if (event.getCategory() == Biome.Category.PLAINS)
-            // event.getSpawns().getSpawner(EntityClassification.MISC).add(new MobSpawnInfo.Spawners(EXAMPLE.get(), 8, 4, 4));
+        // event.getSpawns().getSpawner(EntityClassification.MISC).add(new MobSpawnInfo.Spawners(EXAMPLE.get(), 8, 4, 4));
     }
 
     /* Registry Methods */
@@ -35,13 +36,12 @@ public class ModEntities
      * @param builder        The entity builder
      * @param primaryColor   The egg color of the egg item
      * @param secondaryColor The spot color for the egg item
-     * @param properties     The properties for the egg item
      * @param <T>            The type of entity being created
      */
-    private static <T extends Entity> RegistryObject<EntityType<T>> register(String id, EntityType.Builder<T> builder, int primaryColor, int secondaryColor, Item.Properties properties)
+    private static <T extends Entity> RegistryObject<EntityType<T>> register(String id, EntityType.Builder<T> builder, int primaryColor, int secondaryColor)
     {
         RegistryObject<EntityType<T>> object = register(id, builder);
-        ModItems.ITEMS.register(id + "_spawn_egg", () -> new SpawnEggItemBase<>(object, primaryColor, secondaryColor, true, properties));
+        ModItems.ITEMS.register(id + "_spawn_egg", () -> new SpawnEggItemBase<>(object, primaryColor, secondaryColor, true, new Item.Properties().group(ItemGroup.MISC)));
         return object;
     }
 
